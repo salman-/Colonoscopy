e6a38420f95977750b508689f28d6315156fad69
Salman Lashkarara <salman.lashkarara@mooncascade.com>
2019-10-13 13:57:03 +0200

    obtain states

---
+++ Code/tt.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+
+dict = {"State":np.random.randint(low=1,high=20,size=30)}
+dt = pd.DataFrame(data=dict)
+
+options = np.random.choice([True,False],30)
+
+print(options)
+
+print(dt[options].State)
