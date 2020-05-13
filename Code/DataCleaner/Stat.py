842e45972bd9e5ba48f6a441ac7b592049af0ff3
Salman Lashkarara <salman.lashkarara@mooncascade.com>
2020-05-13 20:50:39 +0200

    Add distribution of states

--- Code/DataCleaner/Stat.py
+++ Code/DataCleaner/Stat.py
@@ -12,17 +12,46 @@ class Stat:
         self.countStateFrquency(self.dt)
 
     def countStateFrquency(self, dt):
+        states = dt["State"].tolist()
+        overallNumberOfStates = len(states)
 
-        dt = dt.loc[:,"State"].tolist()
-        counter = collections.Counter(dt)
-        stat = pd.DataFrame(data=counter, index=["Frquency"])
-        stat = stat.transpose()
-        stat.loc[:, "Probability"] = stat.loc[:, "Frquency"] / len(dt)
-        stat.index.names = ['State']
-        self.saveOutputAsCSV(stat)
+
+        statDT = self.buildDataset()
+        for index, row in statDT.iterrows():
+            statDT.loc[index,'Frequency'] = states.count(row['State'])
+            statDT.loc[index, 'Probability'] = statDT.loc[index,'Frequency']/overallNumberOfStates
+
+
+        statDT.sort_values('stateIndex')
+        self.saveOutputAsCSV(statDT)
+
+    def buildDataset(self):
+        stateList = self.generateStates(25)
+        stateIndex = list(range(len(stateList)))
+        dict = {    "stateIndex" : stateIndex,
+                    "State": stateList   }
+
+        stat = pd.DataFrame(data=dict)
+        stat["Frequency"] = np.nan
+        stat["Probability"] =np.nan
+        return stat
+
+    def generateStates(self, numberOfStates):
+
+        stateList = []
+        for small in range(0, numberOfStates):    # in method1: numberOfStates=7
+            for medium in range(0, numberOfStates):
+                for large in range(0, numberOfStates):
+                    if small + medium + large > (numberOfStates - 1):
+                        break
+                    else:
+                        state = str(small) + "_" + str(medium) + "_" + str(large)
+                        stateList.append(state)
+        #print(stateList)
+        return stateList
 
     def saveOutputAsCSV(self,dt):
-        dt.to_csv(self.outputDTPath, sep=',', encoding='utf-8', index=True)
+        dt.to_csv(self.outputDTPath, sep=',', encoding='utf-8', index=False)
 
     def getDistribution(self,columnName):
 
@@ -32,9 +61,6 @@ class Stat:
         sum = np.sum(values)
         pro = (values/sum).to_frame()
         pro.columns = [ 'Probability']
-        print(type(pro))
-        print(pro.columns)
-        #print(np.sum(pro.tolist()))
         pro.index.names = ['NumberOfPolyps']
         pro.to_csv("./../datasets/"+columnName+"Distribution.csv", sep=',', encoding='utf-8', index=True)
 
