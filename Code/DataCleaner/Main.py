5ecc8a7dbbe34afdba58f399e9f9e97927d70c19
Farahani-Ali <farahani7ali@gmail.com>
2020-06-27 01:25:23 -0500

    the same main

--- Code/DataCleaner/Main.py
+++ Code/DataCleaner/Main.py
@@ -24,16 +24,17 @@ PolypSizeFixMultipleCategory(cleanedDTPath)
 
 
 PolypSizeCleaner(cleanedDTPath)       # Validate the Size of column before obtain the final status
-
+"""
 #DataMergerNoneAdvanced()
+"""
+#DataMerger(0)                         # Merge the seperated capsules in order to obtain the patient status
 
-DataMerger()                         # Merge the seperated capsules in order to obtain the patient status
-DataMerger(1)                        # MergedDT for ADVANCED DT
-DataMerger(2)                        #MergedDT for NON-ADVANCED
+#DataMerger(1)                        # MergedDT for ADVANCED DT
+#DataMerger(2)                        #MergedDT for NON-ADVANCED
 
 #"""
 #----------------------------------------
-
+"""
 inputDTPath  = "./../datasets/Final DT/MergedDT.csv"
 outputDTPath = "./../datasets/Final_CleanedDT.csv"
 
@@ -41,7 +42,7 @@ rsm1 = ReduceStatesMethod1(inputDTPath)   #Obtain the result of each colonscopy 
 #rsm1.labelThePaitentsWithMoreThan6PolypsAs6_6_6()
 rsm1.labelThePaitentsWithCancersAs9_9_9()
 rsm1.output(outputDTPath)
-
+"""
 #----------------------------------------
 """
 inputDTPath  = "./../datasets/Final_CleanedDT.csv"
