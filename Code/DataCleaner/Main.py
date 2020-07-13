63e965fc9fd790697cff462486d82dbf500e3d1e
Farahani-Ali <farahani7ali@gmail.com>
2020-07-13 15:59:09 -0500

    new main with proximal/distal finder

--- Code/DataCleaner/Main.py
+++ Code/DataCleaner/Main.py
@@ -6,29 +6,33 @@ from DataMerger                       import DataMerger
 from ReduceStatesMethod1              import ReduceStatesMethod1
 from CombineRowsWithLessThan6MonthGap import CombineRowsWithLessThan6MonthGap
 from PolypSizeFixMultipleCategory     import PolypSizeFixMultipleCategory
-from FilterInvalidData              import FilterInvalidData
+from FilterInvalidData                import FilterInvalidData
+from proximalAndDistalFinder          import proximalAndDistalFinder
 
 
-originalDTPath = "./../datasets/Original DT/sample11.csv"
+originalDTPath = "./../datasets/Original DT/sample1.csv"
 cleanedDTPath  = "./../datasets/Capsules/cleanedDataSet.csv"
 
-PolypExtractor(originalDTPath)        #Set dataset polyp based
+#PolypExtractor(originalDTPath)        #Set dataset polyp based
 
-QuantificationOfNumberOfSessiles(cleanedDTPath)
+proximalAndDistalFinder(cleanedDTPath)
 
-FilterInvalidData(cleanedDTPath)  #Not needed
+#QuantificationOfNumberOfSessiles(cleanedDTPath)
+
+#FilterInvalidData(cleanedDTPath)  #Not needed
 
 
-PolypSizeFixMultipleCategory(cleanedDTPath)
-"""
-PolypSizeCleaner(cleanedDTPath)       # Validate the Size of column before obtain the final status
+#PolypSizeFixMultipleCategory(cleanedDTPath)
 
-DataMerger(0)                         # Merge the seperated capsules in order to obtain the patient status
+#PolypSizeCleaner(cleanedDTPath)       # Validate the Size of column before obtain the final status, drops "mm" term and
+                           # brings a new column as small,medium,large
+
+#DataMerger(0)                         # Merge the seperated capsules in order to obtain the patient status
 #DataMerger(1)                        # MergedDT for ADVANCED DT
 #DataMerger(2)                        #MergedDT for NON-ADVANCED
 #----------------------------------------
 
-inputDTPath  = "./../datasets/Final DT/MergedDT.csv"
+inputDTPath  = "./../datasets/Final DT/NonAdvancedMergedDT.csv"
 outputDTPath = "./../datasets/Final_CleanedDT.csv"
 
 rsm1 = ReduceStatesMethod1(inputDTPath)   #Obtain the result of each colonscopy as an state
@@ -37,7 +41,7 @@ rsm1.labelThePaitentsWithCancersAs9_9_9()
 rsm1.output(outputDTPath)
 
 #----------------------------------------
-
+"""
 inputDTPath  = "./../datasets/Final_CleanedDT.csv"
 arwlt6mg = CombineRowsWithLessThan6MonthGap(inputDTPath)
 """
